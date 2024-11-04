b4f7c605807d387e90df8f53713a945764849bfd
nimz19 <izzahzafer@gmail.com>
2024-11-04 11:54:49 +0000

    Add hello test

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Izzah"));
+        System.out.println(sayHelloToSomeone("Test"));
         System.out.println(sayGoodbye());
     }
 
